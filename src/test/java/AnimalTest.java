8033c8b99d469a01a518c10d78cfc487f813a612
Nastasyai <141271685+nastasyai@users.noreply.github.com>
2023-10-13 23:18:37 +0300

    Финальная работа по 6 спринту

---
+++ src/test/java/AnimalTest.java
@@ -0,0 +1,43 @@
+import com.example.Animal;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class AnimalTest {
+    @Test
+    public void getFood() throws Exception {
+        Animal testAnimal = new Animal();
+        try {
+            testAnimal.getFood("дракон");
+            Assert.fail("Expected Exception");
+        }
+        catch (Exception mess) {
+            Assert.assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник", mess.getMessage());
+        }
+    }
+
+    @Test
+    public void getFamily() {
+        String family = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+        Animal testAnimal = new Animal();
+        var actual = testAnimal.getFamily();
+        Assert.assertEquals(family, actual);
+    }
+
+    @Test
+    public void eatMeat() throws Exception {
+        Animal testAnimal = new Animal();
+        List<String> responseEatMeat = testAnimal.
+                getFood("Хищник");
+        Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), responseEatMeat);
+    }
+
+    @Test
+    public void notEatMeat() throws Exception {
+        Animal testAnimal = new Animal();
+        List<String> responseNotEatMeat = testAnimal.
+                getFood("Травоядное");
+        Assert.assertEquals(List.of("Трава", "Различные растения"), responseNotEatMeat);
+    }
+}
